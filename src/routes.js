40e6bb2b6726e3703d9ef93c8fc22438e79bbaf3
Matheus Santos alves <matheussantosalves4@gmail.com>
2020-02-08 12:51:00 -0300

    Migration, model e controller de avatar.

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/authentication';
 import multerConfig from './config/multer';
@@ -12,9 +13,7 @@ const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
