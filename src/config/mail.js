17f4050a7d44edb538579f9737f7ee59dc0f7215
Matheus Santos alves <matheussantosalves4@gmail.com>
2020-02-11 11:28:40 -0300

    Adicionando envio de emails com Nodemailer.

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '22879d1faa0af8',
+    pass: 'f9fa9816615719',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
